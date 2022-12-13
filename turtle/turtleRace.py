b452df64431427ed810c48eae47078a8d0a736e2
gmachadom <120364460+gmachadom@users.noreply.github.com>
2022-12-13 19:06:18 -0400

    Update turtleRace.py

--- turtle/turtleRace.py
+++ turtle/turtleRace.py
@@ -3,13 +3,13 @@ from random import choice
 
 
 def race():
-    # Posicionando ambos os jogadores
+    # Setting both players
     player_one.penup()
     player_two.penup()
     player_one.goto(-200, 100)
     player_two.goto(-200, -100)
 
-    # Posicionando as linhas de chegadas
+    # Setting both finish lines
     finish_line_one.penup()
     finish_line_two.penup()
     finish_line_one.goto(200, 100)
@@ -21,12 +21,12 @@ def race():
         player_one_dist = 200 - player_one.xcor()
         player_two_dist = 200 - player_two.xcor()
 
-        # Decidindo o turno de cada jogador
+        # Deciding each player turn
         if rounds % 2 == 0:
-            roll_die = input("Jogador Um, aperte Enter para rodar o dado")
+            roll_die = input("Player One, press Enter key to roll the die")
             outcome = choice(die)
             steps = outcome * 10
-            print(f"O resultado dos dados foi {outcome}\n")
+            print(f"The outcome was {outcome}\n")
 
             if player_one_dist < steps:
                 player_one.forward(player_one_dist)
@@ -34,10 +34,10 @@ def race():
                 player_one.forward(steps)
 
         else:
-            roll_die = input("Jogador Dois, aperte Enter para rodar o dado")
+            roll_die = input("Player Two, press Enter key to roll the die")
             outcome = choice(die)
             steps = outcome * 10
-            print(f"O resultado dos dados foi {outcome}\n")
+            print(f"The outcome was {outcome}\n")
 
             if player_two_dist < steps:
                 player_two.forward(player_two_dist)
@@ -47,23 +47,23 @@ def race():
         rounds += 1
 
     if player_one.xcor() >= 200:
-        print("O JOGADOR UM VENCEU!")
+        print("PLAYER ONE WINS!")
 
     elif player_two.xcor() >= 200:
-        print("O JOGADOR DOIS VENCEU!")
+        print("PLAYER TWO WINS!")
 
 
-# Criando o primeiro jogador
+# Creating player one
 player_one = turtle.Turtle()
 player_one.shape("turtle")
 player_one.color("red")
 
-# Criando o segundo jogador
+# Creating player two
 player_two = turtle.Turtle()
 player_two.shape("turtle")
 player_two.color("blue")
 
-# Criando as linhas de chegadas de cada jogador
+# Creating each player finish line
 finish_line_one = turtle.Turtle()
 finish_line_one.shape("circle")
 finish_line_one.color("pink")
